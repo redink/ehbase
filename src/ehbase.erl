17c2446a56b58fa76f8a3f67c0b0830613b87830
redink <redink@ptthink.com>
2015-03-18 17:13:50 +0800

    [pooler] add  handle branch

--- src/ehbase.erl
+++ src/ehbase.erl
@@ -4,21 +4,26 @@
 
 get(TableName, Row, Column, Timeout) ->
 
-    PidForUse = pooler:take_member(hbase_thrift, Timeout),
-
-    ExecuteResult =
-        case catch gen_server:call(PidForUse,
-                                   {get, [TableName, Row, Column, dict:new()]},
-                                   Timeout) of
-            {ok, Result} ->
-                pooler:return_member(hbase_thrift, PidForUse),
-                {true, Result};
-            _Any ->
-                reset_thrift_conn(PidForUse),
-                pooler:return_member(hbase_thrift, PidForUse),
-                {false, []}
-        end,
-    ExecuteResult.
+    case pooler:take_member(hbase_thrift, Timeout) of
+        error_no_members ->
+            {false, []};
+        PidForUse when erlang:is_pid(PidForUse) ->
+            ExecuteResult =
+                case catch gen_server:call(PidForUse,
+                                           {get, [TableName, Row, Column, dict:new()]},
+                                           Timeout) of
+                    {ok, Result} ->
+                        pooler:return_member(hbase_thrift, PidForUse),
+                        {true, Result};
+                    _Any ->
+                        reset_thrift_conn(PidForUse),
+                        pooler:return_member(hbase_thrift, PidForUse),
+                        {false, []}
+                end,
+            ExecuteResult;
+        _ ->
+            {false, []}
+    end.
 
 reset_thrift_conn(Pid) ->
     gen_server:call(Pid, {reset_connection}).
